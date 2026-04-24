7c6c0656e9a78c06b77a11fdfc4161e05a55ae29
Cucix08-webdev <cucixdev@gmail.com>
2026-04-25 00:09:48 +0200

    Add files via upload

---
+++ Calcolatrice/calcolatrice.js
@@ -0,0 +1,167 @@
+const buttons = document.querySelectorAll(".button");
+const operazione = document.querySelectorAll(".operazione");
+let risultato = document.getElementById("risultato");
+
+let textRisultato = "";
+
+let isElevato = false;
+
+function removeErrorText(text){
+    return (text == "Errore" || text == "Indeterminato" || text == "Numero troppo grande")? "" : text;
+}
+
+let trasparenteApplicata = false;
+
+setInterval(() => {
+    if (textRisultato === "" && !trasparenteApplicata) {
+        operazione.forEach(op => op.classList.add("trasparente"));
+        trasparenteApplicata = true;
+    }
+    else if (textRisultato !== "" && trasparenteApplicata) {
+        operazione.forEach(op => op.classList.remove("trasparente"));
+        trasparenteApplicata = false;
+    }
+}, 100);
+
+buttons.forEach(el => {
+    el.addEventListener("click", () => {
+        switch (el.id) {
+            case "AC":
+                textRisultato = "";
+                break;
+
+            case "C":
+                textRisultato = removeErrorText(textRisultato);
+                textRisultato = textRisultato.slice(0, -1);
+                break;
+        
+            case "potenza":
+                textRisultato = removeErrorText(textRisultato);
+                if (textRisultato !== "") {
+                    textRisultato += "^";
+                }
+                isElevato = true;
+                break;
+
+            case "sqrt":
+                textRisultato = removeErrorText(textRisultato);
+                if (textRisultato !== "") {
+                    textRisultato = String(Math.sqrt(eval(textRisultato)));
+                }
+                break;
+
+            case "negate":
+                textRisultato = removeErrorText(textRisultato);
+                if (textRisultato !== "") {
+                    textRisultato = String(-Number(textRisultato));
+                }
+                break;
+
+            case "log":
+                textRisultato = removeErrorText(textRisultato);
+                if(textRisultato !== ""){
+                    textRisultato = String(Math.log10(Number(textRisultato)))
+                }
+                break;
+
+            case ",":
+                textRisultato = removeErrorText(textRisultato);
+                if (textRisultato !== ""){
+                    textRisultato += ".";
+                }
+                break;
+
+            case "potenza^2":
+                textRisultato = removeErrorText(textRisultato);
+                if (textRisultato !== "") {
+                    textRisultato += "^2";
+                    isElevato = true;
+                }
+                break;
+
+            case "cos":
+                textRisultato = removeErrorText(textRisultato);
+                if (textRisultato !== "") {
+                    textRisultato = String(Math.cos(Number(textRisultato)));
+                }
+                break;
+
+            case "sin":
+                textRisultato = removeErrorText(textRisultato);
+                if (textRisultato !== "") {
+                    textRisultato = String(Math.sin(Number(textRisultato)));
+                }
+                break;
+
+            case "tan":
+                textRisultato = removeErrorText(textRisultato);
+                if (textRisultato !== "") {
+                    textRisultato = String(Math.tan(Number(textRisultato)));
+                }
+                break;
+
+            case "cot":
+                textRisultato = removeErrorText(textRisultato);
+                if (textRisultato !== "") {
+                    textRisultato = String(1/Math.tan(Number(textRisultato)));
+                }
+                break;
+
+            case "sec":
+                textRisultato = removeErrorText(textRisultato);
+                if (textRisultato !== "") {
+                    textRisultato = String(1/Math.cos(Number(textRisultato)));
+                }
+                break;
+
+            case "pi":
+                textRisultato = String(Math.PI);
+                break;
+
+            case "e":
+                textRisultato = String(Math.E);
+                break;
+
+            case "=":
+                textRisultato = removeErrorText(textRisultato);
+                if (isElevato) {
+                    let potenza = textRisultato.indexOf("^")
+                    let x = textRisultato.substring(0, potenza)
+                    let y = textRisultato.substring(potenza + 1)
+
+                    textRisultato = String(Math.pow(Number(x), Number(y)))
+                    
+                    if (Number(x) == 0 && Number(y) == 0){
+                        textRisultato = "Indeterminato";
+                    }
+
+                    isElevato = false;
+                }
+                else if (isElevato == false && textRisultato !== ""){
+                    try{
+                        textRisultato = String(eval(String(textRisultato)))
+                    }
+                    catch (e) {
+                        textRisultato = ""
+                    }
+                }
+                break;
+
+            default:
+                textRisultato = removeErrorText(textRisultato);
+                if(textRisultato !== "" || Number(el.id) >= 0 && Number(el.id) <= 9){
+                    textRisultato += el.innerHTML;
+                }
+                break;
+        }
+
+        if (textRisultato == String(NaN)) {
+            textRisultato = "Errore";
+        }
+        else if (textRisultato == Infinity){
+            textRisultato = "Numero troppo grande"
+        }
+
+        risultato.value = textRisultato
+    });
+});
